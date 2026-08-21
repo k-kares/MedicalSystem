5311e36ba8b2759065e7148af1e93977348ebef1
karlo <karlo.kares7@gmail.com>
2026-08-22 01:51:17 +0200

    Napravljen dbcontext

---
+++ MedicalSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MedicalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Disease> Diseases { get; set; }
+        public DbSet<PatientRecord> PatientRecords { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Examination> Examinations { get; set; }
+    }
+}
