67fe2aa0357486fef2ac4597785f2c5920333f45
karlo <karlo.kares7@gmail.com>
2026-08-23 11:09:59 +0200

    classes to database relations

--- MedicalSystem/Data/ApplicationDbContext.cs
+++ MedicalSystem/Data/ApplicationDbContext.cs
@@ -18,5 +18,56 @@ namespace MedicalSystem.Data
         public DbSet<Medication> Medications { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<Examination> Examinations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Patient>()
+                .HasIndex(p => p.OIB)
+                .IsUnique();
+
+            modelBuilder.Entity<PatientRecord>()
+                .HasOne(pr => pr.Patient)
+                .WithMany()
+                .HasForeignKey(pr => pr.PatientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<PatientRecord>()
+                .HasOne(pr => pr.Disease)
+                .WithMany()
+                .HasForeignKey(pr => pr.DiseaseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Prescription>()
+                .HasOne(p => p.Patient)
+                .WithMany()
+                .HasForeignKey(p => p.PatientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Prescription>()
+                .HasOne(p => p.Doctor)
+                .WithMany()
+                .HasForeignKey(p => p.DoctorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Prescription>()
+                .HasOne(p => p.Medication)
+                .WithMany()
+                .HasForeignKey(p => p.MedicationId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Examination>()
+                .HasOne(e => e.Patient)
+                .WithMany()
+                .HasForeignKey(e => e.PatientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Examination>()
+                .HasOne(e => e.Doctor)
+                .WithMany()
+                .HasForeignKey(e => e.DoctorId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- MedicalSystem/Program.cs
+++ MedicalSystem/Program.cs
@@ -1,11 +1,17 @@
+using MedicalSystem.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.MedicalSystemContext
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
